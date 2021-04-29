f3e7ce2779dd53033cd9dbd7bf905a5ee3e8dab9
Aaron Caito <aaron.caito@gmail.com>
2021-04-29 03:51:06 +0000

    initial

---
+++ jest.config.ts
@@ -0,0 +1,5 @@
+module.exports = {
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  ignorePatterns: ['dist/**', 'target/**', 'src/openapi/**', '.eslintrc.js', 'src/sdk/**'],
+};
